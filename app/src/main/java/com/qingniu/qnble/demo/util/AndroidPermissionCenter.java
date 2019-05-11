87dcdbfb5f959b953c32c307d45d363ad8b00a46
ch <ch@ch.local>
2019-05-11 11:19:17 +0800

    增加双模称demo测试方法

--- app/src/main/java/com/qingniu/qnble/demo/util/AndroidPermissionCenter.java
+++ app/src/main/java/com/qingniu/qnble/demo/util/AndroidPermissionCenter.java
@@ -22,6 +22,7 @@ public class AndroidPermissionCenter {
             "android.permission.ACCESS_FINE_LOCATION"};
 
     public static int REQUEST_EXTERNAL_STORAGE = 201;
+    public static int REQUEST_CAMERA= 202;
 
     //API>=23时才需要判断权限，扫描时再判断权限
     public static void verifyPermissions(Activity activity) {
@@ -38,6 +39,25 @@ public class AndroidPermissionCenter {
                 //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
                 ActivityCompat.requestPermissions(activity,
                         PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+            }
+        }
+    }
+
+    //API>=23时才需要判断权限，扫描时再判断权限
+    public static void verifyCameraPermissions(Activity activity) {
+        //判断是否已经赋予权限
+        if (ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                    Manifest.permission.CAMERA)) {
+                //这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限
+
+            } else {
+                //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
+                ActivityCompat.requestPermissions(activity,
+                        new String[]{Manifest.permission.CAMERA,Manifest.permission.VIBRATE}, REQUEST_CAMERA);
             }
         }
     }
